5603830ba043ce96ca57bd9f378783c7a34d82fb
p1nh0 <p1nh0.c0d1ng@gmail.com>
2013-12-20 21:04:31 +0000

    Minor update
    updated mxj_deployer.js to remove the "o." part of the object when
    deploying the 'mxj' object, since it is not possible to have "." in the
    name of the java class

--- javascript/mxj_deployer.js
+++ javascript/mxj_deployer.js
@@ -5,7 +5,7 @@ var a, c;
 function bang(){
 	// make js object with name argument at object @patching_rect coordinates
 	c = this.patcher.box.rect;// coordinates (@patching_rect)
-	a = this.patcher.parentpatcher.newdefault(c[0], c[1], "mxj", objname);
+	a = this.patcher.parentpatcher.newdefault(c[0], c[1], "mxj", objname.slice(2));
 	
 	// delete js_deployer
 	this.patcher.parentpatcher.remove(this.patcher.box);
